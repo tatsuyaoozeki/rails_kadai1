a79983c652d87887ab1a0e44021ed732db8f40cd
tatsuya oozeki <tasuya6281@icloud.com>
2019-07-31 16:09:09 +0900

    投稿一覧画面作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,9 +1,20 @@
 class PicturesController < ApplicationController
   def index
-
+    @pictures = Picture.all
   end
 
   def new
     @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.create(params[:picture])
+    redirect_to new_picture_path
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content, :image, :image_cahe)
   end
 end
