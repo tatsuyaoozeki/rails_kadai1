62a8bd49c226f1d97f6549952a22ce7a2f022a7e
tatsuya oozeki <tasuya6281@icloud.com>
2019-08-05 16:19:52 +0900

    CSS/bootstrap適用

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap/dist/js/bootstrap.min
+//= require jquery/dist/jquery.js
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
