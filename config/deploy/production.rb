304751abb9d2721d2955315522f77b438db87e12
tatsuya oozeki <tasuya6281@icloud.com>
2019-10-22 14:46:30 +0900

    fix: contig/deploy/production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,6 @@
-server '3.113.180.25', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/tatsuyaoozeki/.ssh/id_rsa'
+server '3.113.180.25',
+    user: 'app',
+    roles: %w{app db web},
+    set :ssh_options: {
+      keys: '/Users/tatsuyaoozeki/.ssh/id_rsa'
+    }
